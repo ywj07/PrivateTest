36a6d7968e4caa8b796d8b6f3aef6802c0dae5a5
ywj07 <245020027@qq.com>
2018-03-20 11:09:13 +0800

    update by yeweijie:20180320_1109,导入root用户下vimrc的配置

---
+++ aa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#***************************************************#
+# ScriptName: aa.sh
+# Author: YEWEIJIE
+# Create Date: 2018-03-20 11:06
+# Modify Author: YEWEIJIE_NEW
+# Modify Date: 2018-03-20 11:07
+# Function: 
+#***************************************************#
+
+
+echo "qaa"
+
+#print{123}         

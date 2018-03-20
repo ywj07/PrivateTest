2bd9be762b8a1a9c7c3df2d5b687068c1ffe0573
ywj07 <245020027@qq.com>
2018-03-20 19:42:30 +0800

    update by yeweijie:20180320_1942,解决vim无法删除问题

---
+++ vim_config/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#***************************************************#
+# ScriptName: test.sh
+# Author: YEWEIJIE
+# Create Date: 2018-03-20 19:33
+# Modify Author: YEWEIJIE_NEW
+# Modify Date: 2018-03-20 19:35
+# Function: 
+#***************************************************#
+
+
+
+         
+
+echo "te测试吖"

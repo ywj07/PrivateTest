bfd9eac581e2853eb623c2f11aaf3f1e21c95dd8
ywj07 <245020027@qq.com>
2018-03-21 18:11:23 +0800

    update by yeweijie:20180321_1811,echo打印文字的颜色配置

---
+++ color_print.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#***************************************************#
+# ScriptName: color_print.sh
+# Author: YEWEIJIE
+# Create Date: 2018-03-21 15:04
+# Modify Author: YEWEIJIE_NEW
+# Modify Date: 2018-03-21 15:27
+# Function: 打印颜色字
+#***************************************************#
+
+echo -e "\033[31m 红色字 \033[0m"
+echo -e "\033[34m 蓝色字 \033[0m"
+
+echo -e "\033[32m 绿色字 \033[0m"
+echo -e "\033[33m 黄色字 \033[0m" 
+
+
+
+echo -e "\033[41;33m 红底黄字 \033[0m"
+echo -e "\033[41;37m 红底白字 \033[0m" 
+

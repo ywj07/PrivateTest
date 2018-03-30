8cf06a4c251431303d06b8ea45edf08356b65a60
ywj07 <245020027@qq.com>
2018-03-30 12:52:16 +0800

    update by yeweijie:20180330_1252,

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#***************************************************#
+# ScriptName: test.sh
+# Author: YEWEIJIE
+# Create Date: 2018-03-28 14:33
+# Modify Author: YEWEIJIE_NEW
+# Modify Date: 2018-03-28 14:33
+# Function: 
+#***************************************************#
+
+for i in {01..10};do
+	echo $i
+done 
+
+           
---
+++ vim_config/aa.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#****************************************************************#
+# ScriptName: aa.sh
+# Author: root
+# Create Date: 2018-03-30 10:33
+# Modify Author: yeweijie
+# Modify Date: 2018-03-30 10:40
+# Function: 
+#***************************************************************#
+
+
+for i in {01..11};do
+	echo $i
+	sleep 1
+	echo "---------------"
+
+done
+
+

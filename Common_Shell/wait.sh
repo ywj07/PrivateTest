757af409fd41654dc7f8e7ad122a16e66a556fdd
ywj07 <245020027@qq.com>
2018-04-02 19:12:06 +0800

    update by yeweijie:20180402_1912,

--- Common_Shell/wait.sh
+++ Common_Shell/wait.sh
@@ -3,18 +3,17 @@
 # ScriptName: wait.sh
 # Author: yeweijie
 # Create Date: 2018-04-02 18:56
-# Modify Author: yeweijie
-# Modify Date: 2018-04-02 18:56
-# Function: 
+# Modify Author: root
+# Modify Date: 2018-04-02 19:03
+# Function: 举例说明wait实现多线程
 #***************************************************************#
 
 #!/bin/bash  
 sleep 10 &  
 echo $!
-sleep 2&
+sleep 2 &
 echo $!  
 wait $! #$!表示上个子进程的进程号，wait等待一个子进程，等待5秒后，退出  
-
 
 # wait命令介绍
 #   wait  [作业指示或进程号]
